df9f18d449377eab270ee6d5fe9b5b261d4f3831
ZiadFahmyZewailCity <s-ziad.fahmy@zewailcity.edu.eg>
2026-06-02 13:26:31 +0300

    more fixes to naming conventions

--- POC_configrable_dashboard/imGUI_Dashboard/build/dashboard.js
+++ POC_configrable_dashboard/imGUI_Dashboard/build/dashboard.js
@@ -580,7 +580,7 @@ function createExportWrapper(name, nargs) {
 var wasmBinaryFile;
 
 function findWasmBinary() {
-  return locateFile('index.wasm');
+  return locateFile('dashboard.wasm');
 }
 
 function getBinarySync(file) {
