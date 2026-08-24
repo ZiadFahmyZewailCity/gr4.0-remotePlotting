5e8a863269b6aba99eacdee72b26c6382ad609aa
ZiadFahmyZewailCity <s-ziad.fahmy@zewailcity.edu.eg>
2026-08-24 21:07:23 +0300

    Add .gitignore and untrack .vscode and build directories

--- build/_deps/packageproject.cmake-build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ziad/GR4/gr4.0-remotePlotting/build/_deps/packageproject.cmake-src
-# Build directory: /home/ziad/GR4/gr4.0-remotePlotting/build/_deps/packageproject.cmake-build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
