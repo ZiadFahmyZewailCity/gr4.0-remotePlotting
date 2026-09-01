20aea5cd5cc11f9d3671817aa06f3a567cb88507
ZiadFahmyZewailCity <s-ziad.fahmy@zewailcity.edu.eg>
2026-09-01 13:56:48 +0300

    Removed some blot that was not actually needed in the repo

--- OOT_Module/imGUI_dashboard_blocks/build/CPM_modules/FindPackageProject.cmake.cmake
+++
@@ -1,3 +0,0 @@
-include("/home/ziad/GNURADIO_DEVELOPMENT/OOT_Module/imGUI_dashboard_blocks/build/cmake/CPM_0.36.0.cmake")
-CPMAddPackage("NAME;PackageProject.cmake;GITHUB_REPOSITORY;TheLartians/PackageProject.cmake;VERSION;1.10.0")
-set(PackageProject.cmake_FOUND TRUE)
--- OOT_Module/imGUI_dashboard_blocks/build/tests/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ziad/GNURADIO_DEVELOPMENT/OOT_Module/imGUI_dashboard_blocks/tests
-# Build directory: /home/ziad/GNURADIO_DEVELOPMENT/OOT_Module/imGUI_dashboard_blocks/build/tests
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test([=[qa_dashBoardServer]=] "/home/ziad/GNURADIO_DEVELOPMENT/OOT_Module/imGUI_dashboard_blocks/build/tests/qa_dashBoardServer")
-set_tests_properties([=[qa_dashBoardServer]=] PROPERTIES  _BACKTRACE_TRIPLES "/home/ziad/GNURADIO_DEVELOPMENT/OOT_Module/imGUI_dashboard_blocks/tests/CMakeLists.txt;36;add_test;/home/ziad/GNURADIO_DEVELOPMENT/OOT_Module/imGUI_dashboard_blocks/tests/CMakeLists.txt;0;")
-subdirs("../_deps/boost_ut-build")
